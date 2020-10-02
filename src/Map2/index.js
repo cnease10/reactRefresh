acba8d017c55d0c03d7815556fca69a8102c1228
cnease10 <cierra.nease10@gmail.com>
2020-10-01 21:45:44 -0700

    simple form that updates state as user types in form fields

--- src/Map2/index.js
+++ src/Map2/index.js
@@ -1,21 +1,35 @@
 import React from 'react';
 
-function Map2() {
-
-    return (
-        <React.Fragment>
+class Map2 extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            text1: "",
+            text2: "",
+            text3: ""
+        }
+       
+    }
+    handleChange = (e) =>  {
+        this.setState({[e.currentTarget.name] : e.target.value})
+        console.log(this.state)
+    }
+    render () {
+        return (
+            <React.Fragment>
             <h2>form practice</h2>
             <form>
-                <input type="text"></input>
+                <input type="text" name="text1" value={this.state.text1} onChange={this.handleChange}></input>
                 <label>text1</label>
-                <input type="text"></input>
+                <input type="text" name="text2" value={this.state.tex2} onChange={this.handleChange}></input>
                 <label>text2</label>
-                <input type="text"></input>
+                <input type="text" name="text3" value={this.state.text3} onChange={this.handleChange}></input>
                 <label>text3</label>
             </form>
         </React.Fragment>
-    )
-        
+    
+        )
+    }     
         
     
 }
