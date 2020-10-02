bd3b0e778ac8b7585a9c95204a38d081cf8af45a
cnease10 <cierra.nease10@gmail.com>
2020-10-02 16:24:41 -0700

    accordion in React

--- src/Map1/index.js
+++ src/Map1/index.js
@@ -10,19 +10,37 @@ function Map1() {
      function headerClick(e) {
         //grab section below and show
         //hide all other sections 
+        clearAcc();
+        e.target.classList.toggle('active')
         console.log('you clicked me')
-        // e.preventDefault();
-        // console.log(e)
+        e.preventDefault();
+        console.log(e.target)
+        let section = e.target.nextElementSibling;
+        console.log(section)
+        section.classList.toggle('active')
+        // const sections = document.querySelectorAll('.sectionAcc')
+        // console.log(sections)
+        
+    }
+    function clearAcc() {
+        let sections = document.querySelectorAll('.sectionAcc')
+        for (let i= 0; i <sections.length; i++) {
+            sections[i].classList.remove('active')
+        }
+        let headers = document.querySelectorAll('.headerTab')
+        for (let x = 0; x < headers.length; x++) {
+            headers[x].classList.remove('active')
+        }
     }
     return (
         <React.Fragment>
 
-            <h1 onClick={(e) => {headerClick(e.target)}}>h1</h1>
-            <section className="section">h1 info</section>
-            <h2 onClick={(e) => {headerClick(e)}}>h2</h2>
-            <section className="section">h2 info</section>
-            <h3 onClick={(e) => {headerClick(e)}}>h3</h3>
-            <section className="section">h3 info</section>
+            <h1 className="headerTab" onClick={headerClick}>h1</h1>
+            <section className="sectionAcc">h1 info</section>
+            <h2  className="headerTab" onClick={headerClick}>h2</h2>
+            <section className="sectionAcc">h2 info</section>
+            <h3  className="headerTab" onClick={headerClick}>h3</h3>
+            <section className="sectionAcc">h3 info</section>
 
         </React.Fragment>
     )
--- src/Map2/index.js
+++ src/Map2/index.js
@@ -21,7 +21,7 @@ class Map2 extends React.Component {
             <form>
                 <input type="text" name="text1" value={this.state.text1} onChange={this.handleChange}></input>
                 <label>text1</label>
-                <input type="text" name="text2" value={this.state.tex2} onChange={this.handleChange}></input>
+                <input type="text" name="text2" value={this.state.text2} onChange={this.handleChange}></input>
                 <label>text2</label>
                 <input type="text" name="text3" value={this.state.text3} onChange={this.handleChange}></input>
                 <label>text3</label>
